7719a9b859b03d42976b6a5a75f27c72b3343845
Danilo Santos <dev.danilosantos@gmail.com>
2024-11-04 19:30:41 -0300

    feat: add initial products dropdown menu

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -1,9 +1,18 @@
 'use client'
 
 import { Badge } from '@/app/_components/ui/badge'
+import { Button } from '@/app/_components/ui/button'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from '@/app/_components/ui/dropdown-menu'
 import { Product } from '@prisma/client'
 import { ColumnDef } from '@tanstack/react-table'
-import { Circle } from 'lucide-react'
+import { Circle, ClipboardCopy, Ellipsis, SquarePen, Trash } from 'lucide-react'
 
 const getStatusLabel = (status: string) => {
     if (status === 'IN_STOCK') {
@@ -50,6 +59,44 @@ export const productsColums: ColumnDef<Product>[] = [
                     <Circle size={10} className="fill-current" />
                     Esgotado
                 </Badge>
+            )
+        },
+    },
+    {
+        accessorKey: 'actions',
+        header: 'Ações',
+        cell({ row }) {
+            const product = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost">
+                            <Ellipsis size={16} />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>Ações</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem
+                            className="gap-1.5"
+                            onClick={() =>
+                                navigator.clipboard.writeText(product.id)
+                            }
+                        >
+                            <ClipboardCopy size={16} />
+                            Copiar ID
+                        </DropdownMenuItem>
+                        <DropdownMenuItem className="gap-1.5">
+                            <SquarePen size={16} />
+                            Editar
+                        </DropdownMenuItem>
+                        <DropdownMenuItem className="gap-1.5">
+                            <Trash size={16} />
+                            Deletar
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             )
         },
     },
