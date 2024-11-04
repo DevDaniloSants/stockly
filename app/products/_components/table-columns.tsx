2a0730bca8c0d246e72e5c9b3b3e026685b55bc0
Danilo Santos <dev.danilosantos@gmail.com>
2024-11-04 20:10:01 -0300

    feat: ensure product can be deleted

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -1,5 +1,9 @@
 'use client'
 
+import {
+    AlertDialog,
+    AlertDialogTrigger,
+} from '@/app/_components/ui/alert-dialog'
 import { Badge } from '@/app/_components/ui/badge'
 import { Button } from '@/app/_components/ui/button'
 import {
@@ -13,6 +17,7 @@ import {
 import { Product } from '@prisma/client'
 import { ColumnDef } from '@tanstack/react-table'
 import { Circle, ClipboardCopy, Ellipsis, SquarePen, Trash } from 'lucide-react'
+import DeleteProductDialogContent from './delete-dialog-content'
 
 const getStatusLabel = (status: string) => {
     if (status === 'IN_STOCK') {
@@ -69,34 +74,39 @@ export const productsColums: ColumnDef<Product>[] = [
             const product = row.original
 
             return (
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost">
-                            <Ellipsis size={16} />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        <DropdownMenuLabel>Ações</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem
-                            className="gap-1.5"
-                            onClick={() =>
-                                navigator.clipboard.writeText(product.id)
-                            }
-                        >
-                            <ClipboardCopy size={16} />
-                            Copiar ID
-                        </DropdownMenuItem>
-                        <DropdownMenuItem className="gap-1.5">
-                            <SquarePen size={16} />
-                            Editar
-                        </DropdownMenuItem>
-                        <DropdownMenuItem className="gap-1.5">
-                            <Trash size={16} />
-                            Deletar
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                <AlertDialog>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="ghost">
+                                <Ellipsis size={16} />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>Ações</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem
+                                className="gap-1.5"
+                                onClick={() =>
+                                    navigator.clipboard.writeText(product.id)
+                                }
+                            >
+                                <ClipboardCopy size={16} />
+                                Copiar ID
+                            </DropdownMenuItem>
+                            <DropdownMenuItem className="gap-1.5">
+                                <SquarePen size={16} />
+                                Editar
+                            </DropdownMenuItem>
+                            <AlertDialogTrigger asChild>
+                                <DropdownMenuItem className="gap-1.5">
+                                    <Trash size={16} />
+                                    Deletar
+                                </DropdownMenuItem>
+                            </AlertDialogTrigger>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <DeleteProductDialogContent productId={product.id} />
+                </AlertDialog>
             )
         },
     },
