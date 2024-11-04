397eed57b47fd27a5053133d697668a6e7c14537
Danilo Santos <dev.danilosantos@gmail.com>
2024-11-03 21:03:04 -0300

    fix: resolve dynamic parameter access issue in ProductsDetails componen

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,7 +2,7 @@ interface Params {
     id: string
 }
 
-const ProductsDetails = async ({ params }: { params: Params }) => {
+const ProductsDetails = async ({ params }: { params: Promise<Params> }) => {
     const { id } = await params
     return <h1>{id}</h1>
 }
