99a1768a42d7772fc4f210aa7212663a2d05c973
Danilo Santos <dev.danilosantos@gmail.com>
2025-01-03 19:11:54 -0300

    feat: ensure revenue are fetched by sale date

--- app/_data-access/dashboard/get-dashboard.ts
+++ app/_data-access/dashboard/get-dashboard.ts
@@ -34,7 +34,8 @@ export const getDashboard = async (): Promise<DashboardDto> => {
             `
             SELECT SUM("unitPrice" * "quantity") as "totalRevenue"
             FROM "SaleProduct"
-            WHERE "createdAt" >= $1 AND "createdAt" <= $2
+            JOIN "Sale" ON "SaleProduct"."saleId" = "Sale"."id"
+            WHERE "Sale"."date" >= $1 AND "Sale"."date" <= $2;
             `,
             day.startOf('day').toDate(),
             day.endOf('day').toDate()
@@ -47,12 +48,13 @@ export const getDashboard = async (): Promise<DashboardDto> => {
 
     const totalRevenueQuery = `
     SELECT SUM("unitPrice" * "quantity") as "totalRevenue"
-    FROM "SaleProduct";
+    FROM "SaleProduct"
+    JOIN "Sale" ON "SaleProduct"."saleId" = "Sale"."id";
     `
     const todayRevenueQuery = `
     SELECT SUM("unitPrice" * "quantity") as "todayRevenue"
     FROM "SaleProduct"
-    WHERE "createdAt" >= $1 AND "createdAt" <= $2;
+    JOIN "Sale" ON "SaleProduct"."saleId" = "Sale"."id";
     `
 
     const startOfDay = new Date(new Date().setHours(0, 0, 0, 0))
